6071f7e23d70ae8ecabf9488ec23e37a7f0c1587
lebanap <abelpan2022@gmail.com>
2024-02-25 22:08:27 +0800

    1.Add sound manager, jump and land sound; 2.Add collisionshape for each state

--- Game_GoGoFoxer/player/player.gd
+++ Game_GoGoFoxer/player/player.gd
@@ -11,6 +11,14 @@ const JUMP_VELOCITY: float = -240.0
 
 @onready var sprite_2d = $Sprite2D
 @onready var animation_player = $AnimationPlayer
+@onready var debug_label = $DebugLabel
+@onready var sound_player = $SoundPlayer
+@onready var collision_idle = $CollisionIdle
+@onready var collision_sneak = $CollisionSneak
+@onready var collision_run = $CollisionRun
+@onready var collision_jump = $CollisionJump
+@onready var collision_fall = $CollisionFall
+
 
 enum PLAYER_STATE {IDLE, RUN, JUMP, FALL, HURT, SNEAK, SPRINT}
 
@@ -28,6 +36,7 @@ func _physics_process(delta):
 	get_action_input()
 	move_and_slide()
 	calculate_states()
+	update_debug_label()
 
 func calculate_gravity(delta) -> void:
 	var gravity_factor = clampf(abs(velocity.y) / 1200.0, 0.0, 1.0)
@@ -66,16 +75,46 @@ func get_action_input() -> void:
 	if Input.is_action_pressed("left"):
 		velocity.x = -player_speed
 		sprite_2d.flip_h = true
+		
 	elif Input.is_action_pressed("right"):
 		velocity.x = player_speed
 		sprite_2d.flip_h = false
 		
 	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		SoundManager.play_clip(sound_player, SoundManager.SOUND_JUMP)
 	
+func disable_collision_shapes(state: PLAYER_STATE) -> void:
+	collision_idle.disabled = true
+	collision_run.disabled = true
+	collision_jump.disabled = true
+	collision_fall.disabled = true
+	collision_sneak.disabled = true
+	
+	match state:
+		PLAYER_STATE.IDLE:
+			collision_idle.disabled = false
+		PLAYER_STATE.RUN:
+			collision_run.disabled = false
+		PLAYER_STATE.SPRINT:
+			collision_run.disabled = false
+		PLAYER_STATE.JUMP:
+			collision_jump.disabled = false
+		PLAYER_STATE.FALL:
+			collision_fall.disabled = false
+		PLAYER_STATE.SNEAK:
+			collision_sneak.disabled = false
+	
+
 func set_state(new_state: PLAYER_STATE) -> void:
 	if new_state == player_state:
 		return
+		
+	disable_collision_shapes(new_state)
+	
+	if player_state == PLAYER_STATE.FALL:
+		if new_state == PLAYER_STATE.IDLE or new_state == PLAYER_STATE.RUN:
+			SoundManager.play_clip(sound_player, SoundManager.SOUND_LAND)
 	
 	player_state = new_state
 	
@@ -92,4 +131,11 @@ func set_state(new_state: PLAYER_STATE) -> void:
 			animation_player.play("sprint")
 		PLAYER_STATE.SNEAK:
 			animation_player.play("sneak")
-			
+		
+func update_debug_label() -> void:
+		debug_label.text = "floor: %s\n%s\n%.0f, %.0f" % [
+			is_on_floor(),
+			PLAYER_STATE.keys()[player_state],
+			velocity.x,
+			velocity.y
+		]
